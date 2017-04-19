45b35a34a221c68f60b79cc28459cd9a1e269461
Максим <thanatormax@gmail.com>
2017-04-19 03:00:29 +0300

    Successful test of one leg, but with some servo error

--- sketch_mar22a/sketch_mar22a.ino
+++ sketch_mar22a/sketch_mar22a.ino
@@ -7,8 +7,28 @@
 
 
 
-Servo test;
+
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+struct Point{
+  float x, y, z;
+  Point()
+  {
+    
+  };
+  Point(float _x, float _y, float _z)
+  {
+    x = _x;
+    y = _y;
+    z = _z;
+  }
+};
+
+class Leg{
+	
+};
+
+
 float sqr(float x)
 {
   return x*x;
@@ -60,33 +80,36 @@ void logback(char* s,float num)
   Serial.println(num);
 }
 
-void testMove(int x, int y,int z)
+void reach(Point& point)
 {//всё в милиметрах
   float startCoxaX = 31.5;
   float startCoxaY = 70; 
   float femurCoxaoffset = 30; //сдвиг от движка femur до coxa
   float femurLength = 56;
-  float tibiaLength = 79;
-  float hDist = sqrt( sqr(x-startCoxaX) +  sqr(y-startCoxaY) );
+  float tibiaLength = 78;
+  float hDist = sqrt( sqr(point.x-startCoxaX) +  sqr(point.y-startCoxaY) );
   logback("Dist to point: ", hDist);
-  float primaryAngle = rad2deg(polarAngle(x-startCoxaX,y-startCoxaY));
+  float primaryAngle = rad2deg(polarAngle(point.x-startCoxaX,point.y-startCoxaY));
   logback("Primary Coxa angle: ", primaryAngle);
-  float cAngle = primaryAngle + 55 ; //55 - угол отклонения координат ноги от общей сетки 5 - погрешность
+  float cAngle = primaryAngle + 55 ; //55 - угол отклонения координат ноги от общей сетки 
   logback("Coxa angle:", cAngle);
   float localDestX = hDist-femurCoxaoffset; 
-  float localDestY = z -20; //оптимальное расстояние от пола 
+  float localDestY = point.z -20; //оптимальное расстояние от пола 
   logback("localdestX:", localDestX);
   logback("localdestY:", localDestY);
   //НАЧАЛО ОПИСАНИЯ ПРЯМОЙ
   float A = -2 * localDestX;
+  logback("A: ", A);
   float B = -2 * localDestY;
+  logback("B: ", B);
   float C = sqr(localDestX) + sqr(localDestY) + sqr(femurLength) - sqr(tibiaLength); 
+  logback("C: ", C);
   //КОНЕЦ ОПИСАНИЯ ПРЯМОЙ
-/*
+
   //ОПИСАНИЕ КОЭФФИЦИЕНТОВ КВАДРАТНОГО УРАВНЕНИЯ
-  float _A = 1+ sqr(B)/A;
-  float _B = 2*C*B/A;
-  float _C = (sqr(C)/A) - sqr(femurLength);
+  float _A = 1+ sqr(B)/sqr(A);
+  float _B = 2*C*B/sqr(A);
+  float _C = (sqr(C)/sqr(A)) - sqr(femurLength);
   //ОКОНЧАНИЕ ОПИСАНИЯ КОЭФФИЦИЕНТОВ КВАДРАТНОГО УРАВНЕНИЯ
   float Y1 = (-_B + sqrt(sqr(_B) - 4*_A*_C)) / (2*_A); 
   float Y2 = (-_B - sqrt(sqr(_B) - 4*_A*_C)) / (2*_A); 
@@ -97,18 +120,20 @@ void testMove(int x, int y,int z)
   logback("JointLocalY: ", jointLocalY);
 
   
-  float femurPrimaryAngle = polarAngle(jointLocalX, jointLocalY);
+  float femurPrimaryAngle = rad2deg(polarAngle(jointLocalX, jointLocalY));
   logback("Primary Femur Angle: ", femurPrimaryAngle);
   float fAngle = femurPrimaryAngle + 45;
   logback("Femur Angle: ", fAngle);
-  float primaryTibiaAngle = polarAngle(localDestX - jointLocalX, localDestY - jointLocalY);
+  float primaryTibiaAngle = rad2deg(polarAngle(localDestX - jointLocalX, localDestY - jointLocalY));
   logback("Primary tibia Angle: ", primaryTibiaAngle);
-  float tAngle = 45 -(primaryTibiaAngle - fAngle);
+  float tAngle = 5+femurPrimaryAngle-(primaryTibiaAngle) ; //Отклонение от оси X считается как отклонение от оси Tibia + отклонение Tibia от X
   logback("Tibia Angle: ", tAngle);
-*/
- // setAngle(0,cAngle);
- // setAngle(1,fAngle);
- // setAngle(2,tAngle);
+
+ setAngle(0,cAngle);
+
+  setAngle(1,fAngle);
+ 
+  setAngle(2,tAngle);
   
 }
 
@@ -119,24 +144,32 @@ void setup() {
   pwm.setPWMFreq(60);
   yield();
   //test.attach(10);
-  
+ // testMove(50,170,0);
+ //Serial.println("First point: ");
+   //testMove(70,150,0);
+     //Serial.println("Second point: ");
+   //testMove(100,150,0);  
+  // Serial.println("Third point: ");
+   // testMove(100,180,0);
+     //Serial.println("4 point: ");
+   // testMove(70,180,0);
+  //delay(2000);
 }
 
 void loop() {
+  /*Serial.println("First point: ");
+   testMove(70,150,5);
+   delay(2000);
+     Serial.println("Second point: ");
+   testMove(100,150,5);  
+   delay(2000);
+   Serial.println("Third point: ");
+    testMove(100,180,5);
+    delay(2000);
+     Serial.println("4 point: ");
+    testMove(70,180,5);
+    delay(2000);*/
 
-  testMove(130,110,0);
-  //setAngle(1,43);
-  //delay(5000);
-  //setAngle(0,testMove(70,200,0));
-  //delay(5000);
- //   setAngle(0,testMove(130,110,0));
- // delay(5000);
-  //  setAngle(0,testMove(90,70,0));
- // delay(5000);
-   // setAngle(0,testMove(80,25,0));
- // delay(5000);
- // test.write(90-testMove(70,70));
-  //delay(3000);
   
 
 }
