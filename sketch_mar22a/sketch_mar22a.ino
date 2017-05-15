dbf9e0b60e447dbde1185f93f2f7f999e5a47e4e
Максим <thanatormax@gmail.com>
2017-05-16 00:41:41 +0300

    Òåïåðü ëåâûå ëàïû íîðìàëüíî îïðåäåëÿþòñÿ

--- sketch_mar22a/sketch_mar22a.ino
+++ sketch_mar22a/sketch_mar22a.ino
@@ -29,23 +29,23 @@ float polarAngle(float x, float y)
 {
     if (x > 0)
         return atan(y / x);
-    
+
     if (x < 0 && y >= 0)
     {
-        
+
         return atan(y / x) + PI;
     }
-    
+
     if (x < 0 && y < 0)
         return atan(y / x) - PI;
-        
-   
+
+
     if (y > 0) // x = 0
         return PI / 2;
-    
+
     if (y < 0)
         return PI / -2;
-    
+
     // y = 0
     return 0;
 }
@@ -60,7 +60,7 @@ struct Point{
   float x, y, z;
   Point()
   {
-    
+
   };
   Point(float _x, float _y, float _z)
   {
@@ -73,28 +73,31 @@ struct Point{
 class Leg{
 private:
 	Point _cPosition;
-	
+
 	Point _legPosition;
-	
+
 	float _cMeshOffset;
 	float _fMeshOffset;
 	float _tMeshOffset;
-	
+
 	float femurCoxaOffset;
-	
+
 	float _fLength;
 	float _tLength;
-	
-	float _cDirectionServo;
-	float _fDirectionServo;
-	float _tDirectionServo;
-	
+
+  float _Direction;
+
+	// float _cDirectionServo;
+	// float _fDirectionServo;
+	// float _tDirectionServo;
+
 	int _cPWMServoPin;
 	int _fPWMServoPin;
 	int _tPWMServoPin;
-	
+
 public:
-Leg(int fPin, int sPin, int tPin, int cMeshOffset, int fMeshOffset, int tMeshOffset, Point& cPosition)
+Leg(int fPin, int sPin, int tPin, float Direction,
+   int cMeshOffset, int fMeshOffset, int tMeshOffset, Point& cPosition)
 {
   _cPWMServoPin = fPin;
   _fPWMServoPin = sPin;
@@ -103,6 +106,8 @@ Leg(int fPin, int sPin, int tPin, int cMeshOffset, int fMeshOffset, int tMeshOff
   _fMeshOffset = fMeshOffset;
   _tMeshOffset = tMeshOffset;
   _cPosition = cPosition;
+  _Direction = Direction;
+
 }
 void setAngle(int servo,float angle){
   uint16_t pulselength = map(angle, 0, 180, SERVOMIN, SERVOMAX);
@@ -112,7 +117,7 @@ void setAngle(int servo,float angle){
 void reach(Point& point)
 {//всё в милиметрах
   float startCoxaX = 31.5;
-  float startCoxaY = 70; 
+  float startCoxaY = 70;
   float femurCoxaoffset = 30; //сдвиг от движка femur до coxa
   float femurLength = 56;
   float tibiaLength = 78;
@@ -122,10 +127,10 @@ void reach(Point& point)
   logback("X:", point.x-this->_cPosition.x);
   logback("Y:", point.y-this->_cPosition.y);
   logback("Primary Coxa angle: ", primaryAngle);
-  float cAngle = primaryAngle + this->_cMeshOffset  ; //55 - угол отклонения координат ноги от общей сетки 
+  float cAngle = primaryAngle + this->_Direction*this->_cMeshOffset  ; //55 - угол отклонения координат ноги от общей сетки
   logback("Coxa angle:", cAngle);
-  float localDestX = hDist-femurCoxaoffset; 
-  float localDestY = point.z -27; //оптимальное расстояние от пола 
+  float localDestX = hDist-femurCoxaoffset;
+  float localDestY = point.z -27; //оптимальное расстояние от пола
   logback("localdestX:", localDestX);
   logback("localdestY:", localDestY);
   //НАЧАЛО ОПИСАНИЯ ПРЯМОЙ
@@ -133,7 +138,7 @@ void reach(Point& point)
   logback("A: ", A);
   float B = -2 * localDestY;
   logback("B: ", B);
-  float C = sqr(localDestX) + sqr(localDestY) + sqr(femurLength) - sqr(tibiaLength); 
+  float C = sqr(localDestX) + sqr(localDestY) + sqr(femurLength) - sqr(tibiaLength);
   logback("C: ", C);
   //КОНЕЦ ОПИСАНИЯ ПРЯМОЙ
 
@@ -142,30 +147,38 @@ void reach(Point& point)
   float _B = 2*C*B/sqr(A);
   float _C = (sqr(C)/sqr(A)) - sqr(femurLength);
   //ОКОНЧАНИЕ ОПИСАНИЯ КОЭФФИЦИЕНТОВ КВАДРАТНОГО УРАВНЕНИЯ
-  float Y1 = (-_B + sqrt(sqr(_B) - 4*_A*_C)) / (2*_A); 
-  float Y2 = (-_B - sqrt(sqr(_B) - 4*_A*_C)) / (2*_A); 
+  float Y1 = (-_B + sqrt(sqr(_B) - 4*_A*_C)) / (2*_A);
+  float Y2 = (-_B - sqrt(sqr(_B) - 4*_A*_C)) / (2*_A);
   float jointLocalY = (Y1 > Y2) ? Y1 : Y2;
   float jointLocalX = ((-C-B*jointLocalY) / A); //ПОЛУЧЕНА ВЕРХНЯЯ ТОЧКА ПЕРЕСЕЧЕНИЯ ПРЯМОЙ И ОКРУЖНОСТИ
 
   logback("JointLocalX: ", jointLocalX);
   logback("JointLocalY: ", jointLocalY);
 
-  
+
   float femurPrimaryAngle = rad2deg(polarAngle(jointLocalX, jointLocalY));
   logback("Primary Femur Angle: ", femurPrimaryAngle);
   float fAngle = femurPrimaryAngle + this->_fMeshOffset;
+  if (this->_Direction == -1)
+  {
+    fAngle = 180 -femurPrimaryAngle - this->_fMeshOffset;
+  }
   logback("Femur Angle: ", fAngle);
   float primaryTibiaAngle = rad2deg(polarAngle(localDestX - jointLocalX, localDestY - jointLocalY));
   logback("Primary tibia Angle: ", primaryTibiaAngle);
-  float tAngle = this->_tMeshOffset+femurPrimaryAngle-(primaryTibiaAngle) ; //Отклонение от оси X считается как отклонение от оси Tibia + отклонение Tibia от X. У Tibia перевернутая сетка.
+  float tAngle = this->_tMeshOffset+femurPrimaryAngle-(primaryTibiaAngle); //Отклонение от оси X считается как отклонение от оси Tibia + отклонение Tibia от X. У Tibia перевернутая сетка.
+  if (this->_Direction == - 1)
+  {
+    tAngle = 180 - (this->_tMeshOffset+femurPrimaryAngle-(primaryTibiaAngle));
+  }
   logback("Tibia Angle: ", tAngle);
 
  setAngle(this->_cPWMServoPin,cAngle);
 
   setAngle(this->_fPWMServoPin,fAngle);
- 
+
   setAngle(this->_tPWMServoPin,tAngle);
-  
+
 }
 
 };
@@ -179,21 +192,19 @@ void setup() {
   pwm.begin();
   pwm.setPWMFreq(60);
   yield();
+
 }
 
 void loop() {
 
-// setAngleNC(2,90);
-Point cPos(31.5,8,0);
-Leg second(0,1,2,93,37,2,cPos);
-Point f(120,10,0);
-Point s(150,10,0);
-Point t(150,40,0);
-Point fo(120,40,0);
-//Point f(70,150,0);
-//Point s(100,150,0);
-//Point t(100,180,0);
-//Point fo(70,180,0);
+
+//setAngleNC(0,90);
+Point cPos(-31.5,-64,0);
+Leg second(0,1,2,-1,130,48,5,cPos);
+Point f(-150,-20,0);
+Point s(-120,-20,0);
+Point t(-120,-50,0);
+Point fo(-150,-50,0);
 Serial.println("First point: ");
 second.reach(f);
 delay(2000);
@@ -207,8 +218,8 @@ Serial.println("4 point: ");
 second.reach(fo);
 delay(2000);
 
- 
 
-  
+
+
 
 }
